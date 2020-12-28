edc89ad427f392052f9dad06e8f1906ca5d34eda
chenziwen <401030526@qq.com>
2020-12-28 11:39:22 +0800

    1.创建了一个SpringBoot项目，往里面添加了Security的依赖

---
+++ anime-login/src/main/java/com/czw/animelogin/AnimeLoginApplication.java
@@ -0,0 +1,13 @@
+package com.czw.animelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeLoginApplication.class, args);
+    }
+
+}
---
+++ anime-login/src/test/java/com/czw/animelogin/AnimeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czw.animelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
